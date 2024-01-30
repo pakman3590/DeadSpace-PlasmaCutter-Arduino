b3ec607fdae29cd787b7483ba5ed8060e83cb764
Matthew Pak <mattpak@gmail.com>
2024-01-29 22:19:06 -0800

    Switched over to faster DFPlayerMini library

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <Servo.h>
 #include <ezButton.h>
 #include <FastLED.h>
-#include <DFRobotDFPlayerMini.h>
+#include <DFPlayerMini_Fast.h>
 
 /*
   INPUTS
@@ -31,7 +31,7 @@ void initializeInputs() {
 */
 
 // Definitions
-DFRobotDFPlayerMini player;
+DFPlayerMini_Fast player;
 
 const int defaultVol = 30;
 
@@ -52,83 +52,12 @@ void playSFX(int track) {
 // Setup Function
 void initializeDFPlayer() {
   Serial1.begin(9600);
-
-  Serial.println();
   Serial.println(F("Initializing DFPlayer... (May take 3-5 seconds)"));
-
-  if (!player.begin(Serial1, true, true)) {
-    Serial.println(F("DFPlayer initialization failed"));
-    Serial.println(F("1. Please recheck the RX/TX connection"));
-    Serial.println(F("2. Please recheck the SD card!"));
-  }
-
+  player.begin(Serial1, true);
   Serial.println(F("DFPlayer Mini initialized."));
 
   player.volume(defaultVol);
 }
-
-// DFPlayer logging
-void printDetail(uint8_t type, int value){
-  switch (type) {
-    case TimeOut:
-      Serial.println(F("Time Out!"));
-      break;
-    case WrongStack:
-      Serial.println(F("Stack Wrong!"));
-      break;
-    case DFPlayerCardInserted:
-      Serial.println(F("Card Inserted!"));
-      break;
-    case DFPlayerCardRemoved:
-      Serial.println(F("Card Removed!"));
-      break;
-    case DFPlayerCardOnline:
-      Serial.println(F("Card Online!"));
-      break;
-    case DFPlayerUSBInserted:
-      Serial.println("USB Inserted!");
-      break;
-    case DFPlayerUSBRemoved:
-      Serial.println("USB Removed!");
-      break;
-    case DFPlayerPlayFinished:
-      Serial.print(F("Number:"));
-      Serial.print(value);
-      Serial.println(F(" Play Finished!"));
-      break;
-    case DFPlayerError:
-      Serial.print(F("DFPlayerError:"));
-      switch (value) {
-        case Busy:
-          Serial.println(F("Card not found"));
-          break;
-        case Sleeping:
-          Serial.println(F("Sleeping"));
-          break;
-        case SerialWrongStack:
-          Serial.println(F("Get Wrong Stack"));
-          break;
-        case CheckSumNotMatch:
-          Serial.println(F("Check Sum Not Match"));
-          break;
-        case FileIndexOut:
-          Serial.println(F("File Index Out of Bound"));
-          break;
-        case FileMismatch:
-          Serial.println(F("Cannot Find File"));
-          break;
-        case Advertise:
-          Serial.println(F("In Advertise"));
-          break;
-        default:
-          break;
-      }
-      break;
-    default:
-      break;
-  }
-}
-
 
 /*
   SERVOS
@@ -141,7 +70,7 @@ void printDetail(uint8_t type, int value){
 // Fin Servo
 Servo finservo;                         // MG90S servo used to actuate retracting fins
 const int finServoPin = 9;
-const int finExtendedPos = 0;           
+const int finExtendedPos = 0;
 const int finRetractedPos = 165;
 
 const int finDelay = 325;               // Approx delay @ 5V
@@ -236,7 +165,7 @@ void ledsOff() {
 void initializeLights() {
   Serial.println(F("Initializing lasers and LEDs"));
   pinMode(laserTransistorPin, OUTPUT);
-  
+
   FastLED.addLeds<NEOPIXEL, ledDataPin>(nozzleLeds, ledNum);
 }
 
@@ -316,7 +245,7 @@ void loop() {
   innerTrigger.loop();
   mainTrigger.loop();
   selectorSwitch.loop();
-  
+
   // Servo
 
   // DFPlayer
